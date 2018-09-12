735586bdc0467f611544951c50bbbc8de2e958e2
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-12 09:56:44 +0200

    Adding README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+#My portfolio
+
+#This is a static portfolio site that I have built using Middleman framework.
+
+#Here is a WIP of the portfolio site
+
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,8 +6,18 @@ describe 'Index Page', type: :feature do
     expect(page).to have_css '.projects'
     within '.projects' do
     expect(page).to have_content 'Projects'
-    expect(page).to have_content 'About me'
+    expect(page).to have_content 'About'
     end
   end
- end
+
+    it 'displays about list' do
+        visit '/'
+
+        expect(page).to have_css '.about'
+        within '.about' do
+        expect(page).to have_content 'About'
+        expect(page).to have_content 'Contact'
+        end
+    end
+end
 
