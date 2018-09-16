7572a7f1433399f88a14448aa54bdf8dfdb76fd2
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-16 11:20:05 +0200

    Trying some js out

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,15 @@
-// This is where it all goes :)
 
+// window.onscroll = function() {myFunction()};
 
+// let header = document.getElementById("myHeader");
+
+// let sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 
